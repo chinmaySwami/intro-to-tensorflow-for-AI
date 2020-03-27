e8d8743b90ea5d4ca8bca5ea6893611d9d676a85
Chinmay Swami <chinmayasami@gmail.com>
2020-03-27 17:45:06 -0400

    completed exercise 5

--- Week 2/Exercise.py
+++ Week 2/Exercise.py
@@ -1,6 +1,5 @@
 # Importing tensor flow and other related libraries
 import tensorflow as tf
-print(tf.__version__)
 import numpy as np
 
 # Retriving the mnist dataset
@@ -13,40 +12,26 @@ mnist = tf.keras.datasets.mnist
 training_images = training_images/2
 test_images = test_images/2
 
-# # creating the NN architecture
-# model = tf.keras.models.Sequential([
-#     tf.keras.layers.Flatten(),  # to get 2D matrix of image and convert it to 1D
-#     tf.keras.layers.Dense(512, activation=tf.nn.relu),  # Hidden layer of 512 neurons
-#     tf.keras.layers.Dense(10, activation=tf.nn.softmax)  # Output layer of 10 neurons because we have 10 classes
-# ])
-#
-# # Compiling the model arch
-# model.compile(optimizer='adam', loss = 'sparse_categorical_crossentropy')
-#
-# # Training the model
-# model.fit(training_images, training_labels, epochs=5)
-#
-# # Testing the model
-# model.evaluate(test_images, test_labels)
-#
-# classifications = model.predict(test_images)
-#
-# print(classifications[0], test_labels[0])
-
-# Adding extra layers
 # creating the NN architecture
 model = tf.keras.models.Sequential([
     tf.keras.layers.Flatten(),  # to get 2D matrix of image and convert it to 1D
     tf.keras.layers.Dense(512, activation=tf.nn.relu),  # Hidden layer of 512 neurons
-    tf.keras.layers.Dense(256, activation=tf.nn.relu),  # Hidden layer of 256 neurons
     tf.keras.layers.Dense(10, activation=tf.nn.softmax)  # Output layer of 10 neurons because we have 10 classes
 ])
 
+# Adding extra layers
+# model = tf.keras.models.Sequential([
+#     tf.keras.layers.Flatten(),  # to get 2D matrix of image and convert it to 1D
+#     tf.keras.layers.Dense(512, activation=tf.nn.relu),  # Hidden layer of 512 neurons
+#     tf.keras.layers.Dense(256, activation=tf.nn.relu),  # Hidden layer of 256 neurons
+#     tf.keras.layers.Dense(10, activation=tf.nn.softmax)  # Output layer of 10 neurons because we have 10 classes
+# ])
+
 # Compiling the model arch
-model.compile(optimizer='adam', loss = 'sparse_categorical_crossentropy')
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy')
 
 # Training the model
-model.fit(training_images, training_labels, epochs=5)
+model.fit(training_images, training_labels, epochs=5, verbose=2)
 
 # Testing the model
 model.evaluate(test_images, test_labels)
@@ -54,4 +39,5 @@ model.evaluate(test_images, test_labels)
 classifications = model.predict(test_images)
 
 print(classifications[0], test_labels[0])
+
 
