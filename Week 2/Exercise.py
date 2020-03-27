ef10bad07335c9b9f66950ccab2184e6be451922
Chinmay Swami <chinmayasami@gmail.com>
2020-03-27 15:57:35 -0400

    completed exercise 5

--- Week 2/Exercise.py
+++ Week 2/Exercise.py
@@ -13,10 +13,32 @@ mnist = tf.keras.datasets.mnist
 training_images = training_images/2
 test_images = test_images/2
 
+# # creating the NN architecture
+# model = tf.keras.models.Sequential([
+#     tf.keras.layers.Flatten(),  # to get 2D matrix of image and convert it to 1D
+#     tf.keras.layers.Dense(512, activation=tf.nn.relu),  # Hidden layer of 512 neurons
+#     tf.keras.layers.Dense(10, activation=tf.nn.softmax)  # Output layer of 10 neurons because we have 10 classes
+# ])
+#
+# # Compiling the model arch
+# model.compile(optimizer='adam', loss = 'sparse_categorical_crossentropy')
+#
+# # Training the model
+# model.fit(training_images, training_labels, epochs=5)
+#
+# # Testing the model
+# model.evaluate(test_images, test_labels)
+#
+# classifications = model.predict(test_images)
+#
+# print(classifications[0], test_labels[0])
+
+# Adding extra layers
 # creating the NN architecture
 model = tf.keras.models.Sequential([
     tf.keras.layers.Flatten(),  # to get 2D matrix of image and convert it to 1D
     tf.keras.layers.Dense(512, activation=tf.nn.relu),  # Hidden layer of 512 neurons
+    tf.keras.layers.Dense(256, activation=tf.nn.relu),  # Hidden layer of 256 neurons
     tf.keras.layers.Dense(10, activation=tf.nn.softmax)  # Output layer of 10 neurons because we have 10 classes
 ])
 
@@ -31,5 +53,5 @@ model.evaluate(test_images, test_labels)
 
 classifications = model.predict(test_images)
 
-print(classifications, test_labels[0])
+print(classifications[0], test_labels[0])
 
