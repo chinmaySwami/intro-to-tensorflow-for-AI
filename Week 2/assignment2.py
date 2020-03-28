bb5fce2bf99cf4b4bd7db79b7b4d5e05c7220107
Chinmay Swami <chinmayasami@gmail.com>
2020-03-28 17:03:18 -0400

    created function that returns a compiled NN model

--- Week 2/assignment2.py
+++ Week 2/assignment2.py
@@ -1,6 +1,21 @@
 import tensorflow as tf
 import numpy as np
 
+
+# Creating a function that would create and return a model
+def create_model():
+    # creating the NN architecture
+    model = tf.keras.models.Sequential([
+        tf.keras.layers.Flatten(),  # to get 2D matrix of image and convert it to 1D
+        tf.keras.layers.Dense(512, activation=tf.nn.relu),  # Hidden layer of 512 neurons
+        tf.keras.layers.Dense(10, activation=tf.nn.softmax)  # Output layer of 10 neurons because we have 10 classes
+    ])
+
+    # Compiling the model arch
+    model.compile(optimizer='adam', loss='sparse_categorical_crossentropy')
+
+    return model
+
 # Download the MNIST dataset
 mnist = tf.keras.datasets.mnist
 
@@ -11,15 +26,7 @@ mnist = tf.keras.datasets.mnist
 training_images = training_images/2
 test_images = test_images/2
 
-# creating the NN architecture
-model = tf.keras.models.Sequential([
-    tf.keras.layers.Flatten(),  # to get 2D matrix of image and convert it to 1D
-    tf.keras.layers.Dense(512, activation=tf.nn.relu),  # Hidden layer of 512 neurons
-    tf.keras.layers.Dense(10, activation=tf.nn.softmax)  # Output layer of 10 neurons because we have 10 classes
-])
-
-# Compiling the model arch
-model.compile(optimizer='adam', loss='sparse_categorical_crossentropy')
+model = create_model()
 
 # Training the model
 model.fit(training_images, training_labels, epochs=5, verbose=2)
