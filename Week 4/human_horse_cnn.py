fe7e3aa035f8ae0bf0ee39ba5584e8966d5eb6d5
Chinmay Swami <chinmayasami@gmail.com>
2020-03-29 19:21:46 -0400

    implemented the code for train and validation image generator[C

--- Week 4/human_horse_cnn.py
+++ Week 4/human_horse_cnn.py
@@ -1,19 +1,26 @@
 import os
 import tensorflow as tf
 from tensorflow.keras.optimizers import RMSprop
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
 
 # pointers to the training directories
-train_horse_dir = os.path.join('/Images/horses')
-train_human_dir = os.path.join('/Images/humans')
+train_horse_dir = os.path.join('./Images/Training/horses')
+train_human_dir = os.path.join('./Images/Training/humans')
+
+# pointers to the training directories
+validation_horse_dir = os.path.join('./Images/Validation/horses')
+validation_human_dir = os.path.join('./Images/Validation/humans')
 
 #  Stats about the images present
 print('total training horse images:', len(os.listdir(train_horse_dir)))
 print('total training human images:', len(os.listdir(train_human_dir)))
+print('total validation horse images:', len(os.listdir(validation_horse_dir)))
+print('total validation human images:', len(os.listdir(validation_human_dir)))
 
 model = tf.keras.models.Sequential([
     tf.keras.layers.Conv2D(16, (3, 3), activation='relu', input_shape=(300, 300, 3)), # 300,300 len ahd hgt and 3 for color
-tf.keras.layers.MaxPooling2D(2, 2),
+    tf.keras.layers.MaxPooling2D(2, 2),
     # The second convolution
     tf.keras.layers.Conv2D(32, (3,3), activation='relu'),
     tf.keras.layers.MaxPooling2D(2,2),
@@ -37,4 +44,37 @@ tf.keras.layers.MaxPooling2D(2, 2),
 # Details about the layers of the model
 print(model.summary())
 
+# Compiling the model
+model.compile(loss='binary_crossentropy',
+              optimizer=RMSprop(lr=0.001),
+              metrics=['accuracy'])
+
+# All images will be rescaled by 1./255
+train_datagen = ImageDataGenerator(rescale=1/255)
+validation_datagen = ImageDataGenerator(rescale=1/255)
+
+
+# Flow training images in batches of 128 using train_datagen generator
+train_generator = train_datagen.flow_from_directory(
+        './Images/Training/',  # This is the source directory for training images
+        target_size=(300, 300),  # All images will be resized to 150x150
+        batch_size=128,
+        # Since we use binary_crossentropy loss, we need binary labels
+        class_mode='binary')
+
+# Flow training images in batches of 128 using train_datagen generator
+validation_generator = validation_datagen.flow_from_directory(
+        './Images/Validation/',  # This is the source directory for training images
+        target_size=(300, 300),  # All images will be resized to 150x150
+        batch_size=32,
+        # Since we use binary_crossentropy loss, we need binary labels
+        class_mode='binary')
+
+history = model.fit(
+      train_generator,
+      steps_per_epoch=8,  # We have 1027 images and batch size is 128 hence 8 [128*8 = 1024] epochs
+      epochs=15,
+      verbose=1,
+      validation_data=validation_generator,
+      validation_steps=8)  # We have 256 val images and batch size of 32 hence 8 [32*8 = 256]
 
