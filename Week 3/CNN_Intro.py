e256bb4e6908f0ae95ccc0752430c41b740f39c6
Chinmay Swami <chinmayasami@gmail.com>
2020-03-29 15:35:37 -0400

    added epochs and 256 neurons

---
+++ Week 3/CNN_Intro.py
@@ -0,0 +1,35 @@
+import tensorflow as tf
+import numpy as np
+from keras.wrappers.scikit_learn import KerasClassifier
+from sklearn.model_selection import GridSearchCV
+
+# Download the MNIST dataset
+mnist = tf.keras.datasets.mnist
+
+# Splitting the dataset into train and test
+(training_images, training_labels),  (test_images, test_labels) = mnist.load_data()
+
+# Reshaping the dataset to fit the needs of the convolutional layer
+training_images=training_images.reshape(60000, 28, 28, 1)
+test_images = test_images.reshape(10000, 28, 28, 1)
+
+# Normalizing the dataset
+training_images = training_images/255
+test_images = test_images/255
+
+# Defining the CNN architecture
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Conv2D(64, (3, 3), activation='relu', input_shape=(28, 28, 1)),
+    tf.keras.layers.MaxPool2D(2, 2),
+    tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+    tf.keras.layers.MaxPool2D(2, 2),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(256, activation='relu'),
+    tf.keras.layers.Dense(10, activation='softmax')
+])
+
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['acc'])
+model.summary()  # prints the layer details of NN
+model.fit(training_images, training_labels, epochs=5)
+
+loss= model.evaluate(test_images, test_labels)
